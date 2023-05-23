c5e5a83d10f83ea8426b6e751497f832d47aa39b
Luis Filipe Barbosa da Silva <a47361@alunos.ipb.pt>
2023-05-23 21:44:09 +0100

    Report and UserReport added

---
+++ ProjectBank/src/Classes/Report/Report.java
@@ -0,0 +1,13 @@
+package Classes.Report;
+
+import Classes.Enum.ReportType;
+
+public interface Report {
+    ReportType getType();
+
+    String getReportContent();
+
+    String ReportDate();
+
+    void generateReport();
+}
