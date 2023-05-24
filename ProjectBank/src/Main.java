083f7e0b7bbd16929cf2575e543bbcfb648db87e
Brunofls8 <bruno.silva.2001@hotmail.com>
2023-05-24 20:33:10 +0100

    Factory Design Pattern

--- ProjectBank/src/Main.java
+++ ProjectBank/src/Main.java
@@ -2,12 +2,14 @@ import Classes.*;
 import Classes.Enum.*;
 import Classes.Report.AccountReport;
 import Classes.Report.BankReport;
+import Classes.Report.ReportFactory;
 import Classes.Report.UserReport;
 
 public class Main {
     public static void main(String[] args) {
 
         Bank Millenium = new Bank("Millenium");
+        Bank BES = new Bank("BES");
         Users Bruno = Millenium.CreateUser("Bruno", 919917503, "bruno@oficial.com");
         Users Diogo = Millenium.CreateUser("Diogo", 910788933, "diogo@email.com");
         Users Luis = Millenium.CreateUser("Luis", 967269754, "luis@gmail.com");
@@ -16,14 +18,22 @@ public class Main {
         Account DiogoAccount = Diogo.CreateAccount(10.0, Currencies.EUR);
         Account LuisAccount = Luis.CreateAccount(1000.0, Currencies.EUR);
 
-        UserReport  Bruno_UserReport = new UserReport(Bruno); //user report test
-        System.out.println(Bruno_UserReport.getReportContent());
+        System.out.println("Bruno Balance: " + BrunoAccount.getTotalBalance());
+        System.out.println("Diogo Balance: " + DiogoAccount.getTotalBalance());
 
-        AccountReport Diogo_AccountReport = new AccountReport(DiogoAccount); //account report test
-        System.out.println(Diogo_AccountReport.getReportContent());
+        BrunoAccount.Deposit(3500,Currencies.PLN);
+        BrunoAccount.Withdraw(500,Currencies.PLN);
+        BrunoAccount.Withdraw(500,Currencies.EUR);
+        BrunoAccount.OpenLoan(2000,"Loan for buying house","2023-06-28" , 0.01 );
 
-        BankReport Millenium_BankReport = new BankReport(Millenium); //bank report test
-        System.out.println(Millenium_BankReport.getReportContent());
+        BrunoAccount.createCard();
+        System.out.println("Bruno Balance: " + BrunoAccount.getTotalBalance());
+        System.out.println("Diogo Balance: " + DiogoAccount.getTotalBalance());
+        DiogoAccount.Transfer(3000,Currencies.PLN,BrunoAccount);
 
+        System.out.println(new ReportFactory().getReport("BANK",Millenium).getReportContent());
+        System.out.println(new ReportFactory().getReport("USER",Bruno).getReportContent());
+        System.out.println(new ReportFactory().getReport("ACCOUNT",BrunoAccount).getReportContent());
+        
     }
 }
