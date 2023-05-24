d544dc1afb9f2bb6524a2f81954ae90ed3c80626
Diogo Nery <a40695@alunos.ipb.pt>
2023-05-24 21:07:26 +0100

    ChainOfResponsibility Design Pattern

--- ProjectBank/src/Main.java
+++ ProjectBank/src/Main.java
@@ -1,13 +1,12 @@
 import Classes.*;
 import Classes.Enum.*;
-import Classes.Report.AccountReport;
-import Classes.Report.BankReport;
-import Classes.Report.UserReport;
 
 public class Main {
     public static void main(String[] args) {
 
         Bank Millenium = new Bank("Millenium");
+        Bank BES = new Bank("BES");
+
         Users Bruno = Millenium.CreateUser("Bruno", 919917503, "bruno@oficial.com");
         Users Diogo = Millenium.CreateUser("Diogo", 910788933, "diogo@email.com");
         Users Luis = Millenium.CreateUser("Luis", 967269754, "luis@gmail.com");
@@ -16,14 +15,18 @@ public class Main {
         Account DiogoAccount = Diogo.CreateAccount(10.0, Currencies.EUR);
         Account LuisAccount = Luis.CreateAccount(1000.0, Currencies.EUR);
 
-        UserReport  Bruno_UserReport = new UserReport(Bruno); //user report test
-        System.out.println(Bruno_UserReport.getReportContent());
+        // Chain Of Responsibility Usage
+        var transfer = new Transfer(1000,Currencies.EUR,BrunoAccount,DiogoAccount);
+        VerificationParameter issamebankParameter = new VerificationParameter.isInSameBank();
+        VerificationParameter currencyParameter = new VerificationParameter.currencyVerifier();
+        VerificationParameter amountParameter = new VerificationParameter.amountVerifier();
 
-        AccountReport Diogo_AccountReport = new AccountReport(DiogoAccount); //account report test
-        System.out.println(Diogo_AccountReport.getReportContent());
+        issamebankParameter.SetNextVerificator(currencyParameter);
+        currencyParameter.SetNextVerificator(amountParameter);
 
-        BankReport Millenium_BankReport = new BankReport(Millenium); //bank report test
-        System.out.println(Millenium_BankReport.getReportContent());
+        System.out.println("BEFORE: \nBalance of Diogo: "+ DiogoAccount.getTotalBalance()+" \nBalance of Bruno: "+ BrunoAccount.getTotalBalance());
+        issamebankParameter.ProcessVerification(transfer); // Triggers Operation
+        System.out.println("AFTER: \nBalance of Diogo: "+ DiogoAccount.getTotalBalance()+" \nBalance of Bruno: "+ BrunoAccount.getTotalBalance());
 
     }
 }
